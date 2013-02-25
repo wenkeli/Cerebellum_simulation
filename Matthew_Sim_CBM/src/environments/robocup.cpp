7f361cf708e75b12d719a57cba435c77587ed8fe
Matthew Hausknecht <matt.localdomain>
2013-02-25 20:37:01 +0000

    Refactored some of the command line handling.

--- Matthew_Sim_CBM/src/environments/robocup.cpp
+++ Matthew_Sim_CBM/src/environments/robocup.cpp
@@ -8,24 +8,30 @@ using namespace std;
 void Robocup::addOptions(boost::program_options::options_description &desc) {
     namespace po = boost::program_options;
     desc.add_options()
-        // ("host", po::value<string>()->default_value("127.0.0.1"), "Robocup: IP of the server")
-        // ("port", po::value<int>()->default_value(20), "Robocup: Server port")
-        // ("rsg", po::value<string>()->default_value("127.0.0.1"), "Robocup: Folder for the nao model")
-        // ("team", po::value<string>()->default_value("127.0.0.1"), "Robocup: Name of team")
+        ("host", po::value<string>()->default_value("127.0.0.1"), "Robocup: IP of the server")
+        ("gPort", po::value<int>()->default_value(3100), "Robocup: Server port")
+        ("mPort", po::value<int>()->default_value(3200), "Robocup: Monitor port")
+        ("uNum", po::value<int>()->default_value(2), "Robocup: Uniform number of the player")
+        ("paramFile", po::value<string>()->default_value("/home/matthew/projects/3Dsim/agents/nao-agent/paramfiles/defaultParams.txt"), "Robocup: Path to the parameter files for the agent.")
+        ("rsg", po::value<string>()->default_value("/usr/local/share/rcssserver3d/rsg/agent/nao"),
+         "Robocup: Folder for the nao model")
+        //("outfile", po::value<string>()->default_value("/tmp/robosim.log"), "Robocup: output file")
+        ("behavior", po::value<string>()->default_value("omniWalkAgent"), "Robocup: Agent behavior")
         ;
 }
 
 
 Robocup::Robocup(CRandomSFMT0 *randGen, boost::program_options::variables_map &vm) : Environment(randGen) {
     robosim.PrintGreeting();
-    robosim.gPort = 3100; // agent-port: The port for the server
-    robosim.mPort = 3200; // server-port: The port for monitor
-    robosim.uNum = 2;
-    robosim.LoadParams("/home/matthew/projects/3Dsim/agents/nao-agent/paramfiles/defaultParams.txt");
-    robosim.rsgdir = "/usr/local/share/rcssserver3d/rsg/agent/nao";
-    robosim.outputFile = "/tmp/out.txt";
+    robosim.mHost      = vm["host"].as<string>();
+    robosim.gPort      = vm["gPort"].as<int>(); // agent-port: The port for the server
+    robosim.mPort      = vm["mPort"].as<int>(); // server-port: The port for monitor
+    robosim.uNum       = vm["uNum"].as<int>();
+    robosim.rsgdir     = vm["rsg"].as<string>();
+    robosim.outputFile = "/dev/null"; // File where robot fitness is written
+    robosim.agentType  = vm["behavior"].as<string>();
+    robosim.LoadParams(vm["paramFile"].as<string>());
     assert(robosim.Init() == true);
-    robosim.agentType = "omniWalkAgent";
     robosim.initializeBehavior();
 }
 
--- Matthew_Sim_CBM/src/main.cpp
+++ Matthew_Sim_CBM/src/main.cpp
@@ -16,31 +16,33 @@ namespace po = boost::program_options;
 int main(int argc, char **argv)
 {
     // Declare the supported options.
-    po::options_description desc("Allowed options");
+    po::options_description desc("Program Usage");
     desc.add_options()
         ("help,h", "produce help message")
+        ("environment,e", po::value<string>()->required(),
+         "Experimental Environment. Choices: default, eyelid, cartpole, robocup")
         ("conPF", po::value<string>()->default_value("../CBM_Params/conParams.txt"),
          "Connectivity Parameter File")
         ("actPF", po::value<string>()->default_value("../CBM_Params/actParams1.txt"),
          "Activity Parameter File")
         ("seed", po::value<int>(), "Random Seed")
         ("nogui", "Run without a gui")
-        ("environment", po::value<string>()->default_value("default"),
-         "Experimental Environment. Choices: default, eyelid, cartpole, robocup")
         ;
     // Allow the environments to add command line args
+    // TODO: Consider allowing each different environment to have its own options set
     Eyelid::addOptions(desc);
     Cartpole::addOptions(desc);
     Robocup::addOptions(desc);
 
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
-    po::notify(vm);
 
-    if (vm.count("help")) {
+    if (vm.count("help") || !vm.count("environment")) {
         cout << desc << "\n";
         return 1;
     }
+
+    po::notify(vm);
 
     int randSeed  = vm.count("seed") ? vm["seed"].as<int>() : -1;
     if (randSeed >= 0) {
