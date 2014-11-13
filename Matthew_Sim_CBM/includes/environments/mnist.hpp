dda7f5ebb1be2f43083e5bbd304330101a423e36
Matthew Hausknecht <matt.localdomain>
2014-11-13 23:40:01 +0000

    More changes to mnist.

--- Matthew_Sim_CBM/includes/environments/mnist.hpp
+++ Matthew_Sim_CBM/includes/environments/mnist.hpp
@@ -11,7 +11,7 @@
 
 class MNist : public Environment {
  public:
-  enum trainPhase { resting, playing };
+  enum trainPhase { resting, training };
 
   MNist(CRandomSFMT0 *randGen, int argc, char **argv);
   ~MNist();
@@ -23,28 +23,37 @@ class MNist : public Environment {
   bool terminated();
   static boost::program_options::options_description getOptions();
 
+  // Returns MNist image converted to MF firing frequency.
+  float* getMNistImageAsMFInput();
+
  protected:
   void readDataset(std::string path);
 
   const static int numMZ = 2;
-
   Microzone *discipleMZ; // The MZ being trained
-
   std::ofstream logfile;
   Microzone mz0, mz1;
-  StateVariable<MNist> sv_highFreq, sv_fft;
-
+  StateVariable<MNist> sv_highFreq, sv_image;
   static const bool randomizeMFs = false;
-
   // Outputs for average force
   std::vector<float> mzOutputs[numMZ];
-
   long phaseTransitionTime;
   trainPhase phase;
+  int image_index_;
 
-  const static int nTrials = 400;
-  const static int nTest = 10;
+  static const int phaseDuration = 2000;
+  static const int restTimeMSec = 1500;
 
-  static const double rest_time_secs = 2;
+  const static int n_train_images_ = 60000;
+  const static int n_test_images_ = 10000;
+  const static int image_rows_ = 28;
+  const static int image_cols_ = 28;
+  unsigned char mnist_train_images[n_train_images_][image_rows_][image_cols_];
+  unsigned char mnist_train_labels[n_train_images_];
+  unsigned char mnist_test_images[n_test_images_][image_rows_][image_cols_];
+  unsigned char mnist_test_labels[n_test_images_];
+
+  const static int num_mf_inputs_ = image_rows_ * image_cols_;
+  float mf_inputs[num_mf_inputs_];
 };
 #endif // MNIST_HPP
--- Matthew_Sim_CBM/src/environments/mnist.cpp
+++ Matthew_Sim_CBM/src/environments/mnist.cpp
@@ -22,8 +22,8 @@ MNist::MNist(CRandomSFMT0 *randGen, int argc, char **argv)
       mz0("mz0", 0, 1, 1, .95),
       mz1("mz1", 1, 1, 1, .95),
       sv_highFreq("highFreqMFs", HIGH_FREQ, .03),
-      sv_fft("audioFreqMFs", MANUAL, .5),
-      phase(resting)
+      sv_image("ImageMFs", MANUAL, float(num_mf_inputs_/2048.0)),
+      phase(resting), image_index_(0)
 {
   po::options_description desc = getOptions();
   po::variables_map vm;
@@ -34,7 +34,7 @@ MNist::MNist(CRandomSFMT0 *randGen, int argc, char **argv)
 
   assert(stateVariables.empty());
   stateVariables.push_back((StateVariable<Environment>*) (&sv_highFreq));
-  stateVariables.push_back((StateVariable<Environment>*) (&sv_fft));
+  stateVariables.push_back((StateVariable<Environment>*) (&sv_image));
 
   assert(microzones.empty());
   microzones.push_back(&mz0);
@@ -42,6 +42,10 @@ MNist::MNist(CRandomSFMT0 *randGen, int argc, char **argv)
 
   // Load the Dataset
   readDataset(vm["mnist_dir"].as<string>().c_str());
+
+  for (int i = 0; i < num_mf_inputs_; ++i) {
+    mf_inputs[i] = 0;
+  }
 }
 
 MNist::~MNist() {
@@ -51,6 +55,8 @@ MNist::~MNist() {
 void MNist::setupMossyFibers(CBMState *simState) {
   Environment::setupMossyFibers(simState);
   Environment::setupStateVariables(randomizeMFs, logfile);
+
+  sv_image.initializeManual(this, &MNist::getMNistImageAsMFInput);
 }
 
 float* MNist::getState() {
@@ -59,11 +65,7 @@ float* MNist::getState() {
   }
 
   for (uint i=0; i<stateVariables.size(); i++) {
-    if (stateVariables[i]->type == HIGH_FREQ) {
-      ;
-    } else {
-      stateVariables[i]->update();
-    }
+    stateVariables[i]->update();
   }
 
   return &mfFreq[0];
@@ -73,14 +75,33 @@ void MNist::step(CBMSimCore *simCore) {
   Environment::step(simCore);
 
   if (phase == resting) {
-    ;
-  } else {
-    ;
+    if (timestep - phaseTransitionTime >= restTimeMSec) {
+      phase = training;
+      phaseTransitionTime = timestep;
+
+      // Queue up the next mnist digit
+      for (int i = 0; i < image_rows_; ++i) {
+        for (int j = 0; j < image_cols_; ++j) {
+          mf_inputs[i * image_cols_ + j] =
+              mnist_train_images[image_index_][i][j] / (float)255;
+        }
+      }
+      image_index_++;
+    }
+  } else { // Training Phase
+    if (timestep - phaseTransitionTime >= phaseDuration) {
+      phase = resting;
+      phaseTransitionTime = timestep;
+      // Clear the mf_inputs
+      for (int i = 0; i < num_mf_inputs_; ++i) {
+        mf_inputs[i] = 0;
+      }
+    }
   }
 }
 
 bool MNist::terminated() {
-  return false;
+  return image_index_ >= 5;
 }
 
 int reverseInt (int i) {
@@ -92,32 +113,104 @@ int reverseInt (int i) {
   return ((int)c1 << 24) + ((int)c2 << 16) + ((int)c3 << 8) + c4;
 }
 
+float* MNist::getMNistImageAsMFInput() {
+  return mf_inputs;
+}
+
 void MNist::readDataset(string mnist_path) {
-  path p(mnist_path);
-  p /= "train-images-idx3-ubyte";
-  assert(exists(p));
-  std::ifstream file(p.c_str(), std::ios::binary);
-  assert(file.is_open());
-  int magic_number, number_of_images, n_rows, n_cols;
-  file.read((char*)&magic_number,sizeof(magic_number));
-  magic_number= reverseInt(magic_number);
-  file.read((char*)&number_of_images,sizeof(number_of_images));
-  number_of_images= reverseInt(number_of_images);
-  file.read((char*)&n_rows,sizeof(n_rows));
-  n_rows= reverseInt(n_rows);
-  file.read((char*)&n_cols,sizeof(n_cols));
-  n_cols= reverseInt(n_cols);
-  assert(magic_number == 2051);
-  assert(number_of_images == 60000);
-  assert(n_rows == 28);
-  assert(n_cols == 28);
-  for(int i = 0; i < number_of_images; ++i) {
-    for(int r = 0; r < n_rows; ++r) {
-      for(int c = 0; c < n_cols; ++c) {
-        unsigned char temp;
-        file.read((char*)&temp,sizeof(temp));
+  { // Load the train images
+    path p(mnist_path);
+    p /= "train-images-idx3-ubyte";
+    assert(exists(p));
+    std::ifstream file(p.c_str(), std::ios::binary);
+    assert(file.is_open());
+    int magic_number, number_of_images, n_rows, n_cols;
+    file.read((char*)&magic_number,sizeof(magic_number));
+    magic_number= reverseInt(magic_number);
+    file.read((char*)&number_of_images,sizeof(number_of_images));
+    number_of_images= reverseInt(number_of_images);
+    file.read((char*)&n_rows,sizeof(n_rows));
+    n_rows= reverseInt(n_rows);
+    file.read((char*)&n_cols,sizeof(n_cols));
+    n_cols= reverseInt(n_cols);
+    assert(magic_number == 2051);
+    assert(number_of_images == n_train_images_);
+    assert(n_rows == image_rows_);
+    assert(n_cols == image_cols_);
+    for(int i = 0; i < number_of_images; ++i) {
+      for(int r = 0; r < n_rows; ++r) {
+        for(int c = 0; c < n_cols; ++c) {
+          file.read((char*)&mnist_train_images[i][r][c],sizeof(char));
+        }
       }
     }
+    file.close();
   }
-  file.close();
+
+  { // Load the training labels
+    path p(mnist_path);
+    p /= "train-labels-idx1-ubyte";
+    assert(exists(p));
+    ifstream file(p.c_str(), std::ios::binary);
+    assert(file.is_open());
+    int magic_number, number_of_images;
+    file.read((char*)&magic_number,sizeof(magic_number));
+    magic_number= reverseInt(magic_number);
+    file.read((char*)&number_of_images,sizeof(number_of_images));
+    number_of_images= reverseInt(number_of_images);
+    assert(magic_number == 2049);
+    assert(number_of_images == n_train_images_);
+    for(int i = 0; i < number_of_images; ++i) {
+      file.read((char*)&mnist_train_labels[i],sizeof(char));
+    }
+    file.close();
+  }
+
+  { // Load the test images
+    path p(mnist_path);
+    p /= "t10k-images-idx3-ubyte";
+    assert(exists(p));
+    std::ifstream file(p.c_str(), std::ios::binary);
+    assert(file.is_open());
+    int magic_number, number_of_images, n_rows, n_cols;
+    file.read((char*)&magic_number,sizeof(magic_number));
+    magic_number= reverseInt(magic_number);
+    file.read((char*)&number_of_images,sizeof(number_of_images));
+    number_of_images= reverseInt(number_of_images);
+    file.read((char*)&n_rows,sizeof(n_rows));
+    n_rows= reverseInt(n_rows);
+    file.read((char*)&n_cols,sizeof(n_cols));
+    n_cols= reverseInt(n_cols);
+    assert(magic_number == 2051);
+    assert(number_of_images == n_test_images_);
+    assert(n_rows == image_rows_);
+    assert(n_cols == image_cols_);
+    for(int i = 0; i < number_of_images; ++i) {
+      for(int r = 0; r < n_rows; ++r) {
+        for(int c = 0; c < n_cols; ++c) {
+          file.read((char*)&mnist_test_images[i][r][c],sizeof(char));
+        }
+      }
+    }
+    file.close();
+  }
+
+  { // Load the test labels
+    path p(mnist_path);
+    p /= "t10k-labels-idx1-ubyte";
+    assert(exists(p));
+    ifstream file(p.c_str(), std::ios::binary);
+    assert(file.is_open());
+    int magic_number, number_of_images;
+    file.read((char*)&magic_number,sizeof(magic_number));
+    magic_number= reverseInt(magic_number);
+    file.read((char*)&number_of_images,sizeof(number_of_images));
+    number_of_images= reverseInt(number_of_images);
+    assert(magic_number == 2049);
+    assert(number_of_images == n_test_images_);
+    for(int i = 0; i < number_of_images; ++i) {
+      file.read((char*)&mnist_test_labels[i],sizeof(char));
+    }
+    file.close();
+  }
 }
