8741a68aca7709a6d9bcc43c8f482e49a61064f5
Matthew Hausknecht <matt.localdomain>
2014-11-20 18:25:23 +0000

    Finalized experimental setup for mnist.

--- Matthew_Sim_CBM/includes/environments/mnist.hpp
+++ Matthew_Sim_CBM/includes/environments/mnist.hpp
@@ -11,7 +11,8 @@
 
 class MNist : public Environment {
  public:
-  enum trainPhase { resting, training };
+  enum trainPhase { resting, learning };
+  enum trainType { train, test };
 
   MNist(CRandomSFMT0 *randGen, int argc, char **argv);
   ~MNist();
@@ -28,22 +29,32 @@ class MNist : public Environment {
 
  protected:
   void readDataset(std::string path);
+  void loadNextImage();
 
-  const static int numMZ = 2;
-  Microzone *discipleMZ; // The MZ being trained
+  const static int numMZ = 1;
   std::ofstream logfile;
-  Microzone mz0, mz1;
+  Microzone mz0;
   StateVariable<MNist> sv_highFreq, sv_image;
-  static const bool randomizeMFs = false;
-  // Outputs for average force
-  std::vector<float> mzOutputs[numMZ];
-  long phaseTransitionTime;
+  static const bool randomizeMFs = true;
+  std::vector<float> mzOutputs; // Outputs for average force
+  long phaseTransitionTime; // Timestep that transition occurred
   trainPhase phase;
-  int image_index_;
+  trainType mode;
+  int target_digit_; // The digit that is being learned
+  // These hold the indexes of images in the training set that
+  // do/don't contain the target digit.
+  std::deque<int> target_indexes_, nontarget_indexes_;
+  int curr_label_;
+  int test_image_indx_;
 
-  static const int phaseDuration = 2000;
-  static const int restTimeMSec = 1500;
+  // Training information
+  static const int phaseDuration = 500;
+  static const int restTimeMSec = 500;
+  const static int trialLen = phaseDuration + restTimeMSec;
+  const static int nTrials = 1000;
+  const static int nTestTrials = 1000;
 
+  // MNist datasets
   const static int n_train_images_ = 60000;
   const static int n_test_images_ = 10000;
   const static int image_rows_ = 28;
--- Matthew_Sim_CBM/src/environments/mnist.cpp
+++ Matthew_Sim_CBM/src/environments/mnist.cpp
@@ -13,6 +13,10 @@ po::options_description MNist::getOptions() {
       ("logfile", po::value<string>()->default_value("mnist.log"),"log file")
       ("mnist_dir", po::value<string>()->required()->default_value("mnist"),
        "MNist dataset directory.")
+      ("target_digit", po::value<int>()->required()->default_value(0),
+       "MNist Digit between [0..9] to learn.")
+      ("mode", po::value<string>()->required()->default_value("train"),
+       "train or test")
       ;
   return desc;
 }
@@ -20,10 +24,9 @@ po::options_description MNist::getOptions() {
 MNist::MNist(CRandomSFMT0 *randGen, int argc, char **argv)
     : Environment(randGen),
       mz0("mz0", 0, 1, 1, .95),
-      mz1("mz1", 1, 1, 1, .95),
       sv_highFreq("highFreqMFs", HIGH_FREQ, .03),
       sv_image("ImageMFs", MANUAL, float(num_mf_inputs_/2048.0)),
-      phase(resting), image_index_(0)
+      phase(resting), curr_label_(-1), test_image_indx_(0)
 {
   po::options_description desc = getOptions();
   po::variables_map vm;
@@ -38,10 +41,35 @@ MNist::MNist(CRandomSFMT0 *randGen, int argc, char **argv)
 
   assert(microzones.empty());
   microzones.push_back(&mz0);
-  microzones.push_back(&mz1);
+
+  // Parse the mode
+  string mode_str = vm["mode"].as<string>();
+  if (mode_str == "train") {
+    mode = train;
+  } else if (mode_str == "test") {
+    mode = test;
+  } else {
+    cerr << "Unknown training type specified: " << mode_str << endl;
+    exit(1);
+  }
 
   // Load the Dataset
   readDataset(vm["mnist_dir"].as<string>().c_str());
+
+  target_digit_ = vm["target_digit"].as<int>();
+  cout << "Learning target_digit " << target_digit_ << endl;
+  assert(target_digit_ < 10); // MNist digits only go [0..9]
+
+  // Separate the training set into relevant and non-relevant digits
+  if (mode == train) {
+    for (int i = 0; i < n_train_images_; ++i) {
+      if (mnist_train_labels[i] == target_digit_) {
+        target_indexes_.push_back(i);
+      } else {
+        nontarget_indexes_.push_back(i);
+      }
+    }
+  }
 
   for (int i = 0; i < num_mf_inputs_; ++i) {
     mf_inputs[i] = 0;
@@ -71,25 +99,57 @@ float* MNist::getState() {
   return &mfFreq[0];
 }
 
+void MNist::loadNextImage() {
+  if (mode == train) {
+    int image_indx;
+    if (curr_label_ != target_digit_) {
+      assert(!target_indexes_.empty());
+      image_indx = target_indexes_.front();
+      target_indexes_.pop_front();
+    } else {
+      assert(!nontarget_indexes_.empty());
+      image_indx = nontarget_indexes_.front();
+      nontarget_indexes_.pop_front();
+    }
+    for (int i = 0; i < image_rows_; ++i) {
+      for (int j = 0; j < image_cols_; ++j) {
+        mf_inputs[i * image_cols_ + j] =
+            mnist_train_images[image_indx][i][j] / (float)255;
+      }
+    }
+    curr_label_ = int(mnist_train_labels[image_indx]);
+  } else if (mode == test) {
+    for (int i = 0; i < image_rows_; ++i) {
+      for (int j = 0; j < image_cols_; ++j) {
+        mf_inputs[i * image_cols_ + j] =
+            mnist_test_images[test_image_indx_][i][j] / (float)255;
+      }
+    }
+    curr_label_ = int(mnist_test_labels[test_image_indx_]);
+    test_image_indx_++;
+  }
+  return;
+}
+
 void MNist::step(CBMSimCore *simCore) {
   Environment::step(simCore);
 
   if (phase == resting) {
     if (timestep - phaseTransitionTime >= restTimeMSec) {
-      phase = training;
+      phase = learning;
       phaseTransitionTime = timestep;
-
-      // Queue up the next mnist digit
-      for (int i = 0; i < image_rows_; ++i) {
-        for (int j = 0; j < image_cols_; ++j) {
-          mf_inputs[i * image_cols_ + j] =
-              mnist_train_images[image_index_][i][j] / (float)255;
-        }
-      }
-      image_index_++;
+      loadNextImage();
     }
-  } else { // Training Phase
+  } else if (phase == learning) {
+    if (mode == test &&
+        (timestep - phaseTransitionTime == phaseDuration - 150)) {
+      // Record the output force 150ms before the error arrival
+      mzOutputs.push_back(mz0.getMovingAverage());
+    }
     if (timestep - phaseTransitionTime >= phaseDuration) {
+      if (curr_label_ == target_digit_) {
+        mz0.smartDeliverError();
+      }
       phase = resting;
       phaseTransitionTime = timestep;
       // Clear the mf_inputs
@@ -97,11 +157,24 @@ void MNist::step(CBMSimCore *simCore) {
         mf_inputs[i] = 0;
       }
     }
+  } else {
+    cerr << "Unknown Phase: " << phase << endl;
+    exit(1);
   }
 }
 
 bool MNist::terminated() {
-  return image_index_ >= 5;
+  if (mode == train && timestep >= nTrials * trialLen) {
+    return true;
+  } else if (mode == test && timestep >= nTestTrials * trialLen) {
+    logfile << "Target Digit: " << target_digit_ << endl;
+    for (uint i = 0; i < mzOutputs.size(); ++i) {
+      logfile << "Label: " << int(mnist_test_labels[i])
+              << " Output: " << mzOutputs[i] << endl;
+    }
+    return true;
+  }
+  return false;
 }
 
 int reverseInt (int i) {
